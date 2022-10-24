2d34e769db1bf46ffe1e8d2489c9c319fb60068f
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-10-25 04:14:53 +0600

    login and signup page design

--- src/Auth/Login/Login.js
+++ src/Auth/Login/Login.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            Log In
+        <div className="artboard phone-2 mx-auto my-5 bg-[#150050] rounded text-white p-5">
+        <h1 className='text-4xl p-4'>Log In</h1>
+        <form>
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Your Email</span>
+                </label>
+                <label className="input-group">
+                    <span>Email</span>
+                    <input type="text" name='email' placeholder="info@site.com" className="input input-bordered w-full" />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Your Password</span>
+                </label>
+                <label className="input-group">
+                    <span>Password</span>
+                    <input type="password" name='password' placeholder="90MKide3#@)" className="input input-bordered w-full" />
+                </label>
+            </div>
+            <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-success m-5" >Log In</button>
+
+        </form>
+
+        <h2 className='p-2 text-xl'>OR</h2>
+        <hr className='w-52 m-auto' />
+
+        <div className="popupLogin p-5">
+
+            <button className="btn btn-warning btn-square m-2">
+                <i class="fa fa-google" aria-hidden="true"></i>
+            </button>
+            <button className="btn btn-square m-2">
+                <i class="fa fa-github" aria-hidden="true"></i>
+            </button>
+            <button className="btn btn-primary btn-square m-2">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </button>
+            <button className="btn btn-success btn-square m-2">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+            </button>
         </div>
+        <p>Didn't have account <Link className='font-bold' to='/signUp'>Sign Up</Link></p>
+
+    </div>
     );
 };
 
--- src/Auth/SignUp/SignUp.js
+++ src/Auth/SignUp/SignUp.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            Sign Up
+        <div className="artboard phone-5 mx-auto bg-[#16003B] rounded text-white my-5 p-5">
+            <h1 className='text-4xl p-4'>Sign Up</h1>
+            <form>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Your Name</span>
+                    </label>
+                    <label className="input-group">
+                        <span>Name</span>
+                        <input type="text" placeholder="Mahmudul Hasan " name='name' className="input input-bordered w-full" />
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Your Email</span>
+                    </label>
+                    <label className="input-group">
+                        <span>Email</span>
+                        <input type="text" placeholder="info@site.com" name='email' className="input input-bordered w-full" />
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Your Password</span>
+                    </label>
+                    <label className="input-group">
+                        <span>Password</span>
+                        <input type="password" placeholder="Password" name='password' className="input input-bordered w-full" />
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Your Profile Picture</span>
+                    </label>
+                    <label className="input-group">
+                        <span>Photo</span>
+                        <input type="file" placeholder="-128390091" name='phone' className="input input-bordered w-full" />
+                    </label>
+                </div>
+                <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-success m-5">Sign Up</button>
+            </form>
+
+            <h2 className='p-2 text-xl'>OR</h2>
+            <hr className='w-52 m-auto' />
+
+            <div className="popupLogin p-5">
+
+                <button className="btn btn-warning btn-square m-2">
+                    <i class="fa fa-google" aria-hidden="true"></i>
+                </button>
+                <button className="btn btn-square m-2">
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                </button>
+                <button className="btn btn-primary btn-square m-2">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                </button>
+                <button className="btn btn-success btn-square m-2">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                </button>
+            </div>
+            <p>Already have an  account <Link className='font-bold' to='/login'>Log In</Link></p>
         </div>
     );
 };
