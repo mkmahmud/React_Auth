e441cfeb0cfc37b26dca6ae03d7cc2e31f7fb7dd
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-10-26 04:23:00 +0600

    set all courses

--- src/Courses/Courses.js
+++ src/Courses/Courses.js
@@ -3,33 +3,49 @@ import { Link, useLoaderData } from 'react-router-dom';
 import Mainbar from './Mainbar';
 import Sidebar from './Sidebar';
 import SingelCourse from './SingelCourses/SingelCourse';
-
+import './Courses.css'
 
 const Courses = () => {
     const APIcategory = useLoaderData();
-    const [courses, setCourses] = useState([])
+    const [courses, setCourses] = useState([]);
+    // Stored courses
+    const [maincourses, setMaincourses] = useState([]) 
 
-    useEffect( ( ) => {
+    // Load All Data
+    useEffect(() => {
         fetch('http://localhost:5000/courses')
-         .then(res => res.json())
-         .then(data => setCourses(data))
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                setMaincourses(data);
+                setCourses(data);
+            })
+    }, [])
 
-    console.log(courses)
+
+    // After Selecting category 
+    const handelcategory = (id) => { 
+            const selectedCategoryData = maincourses.filter(course => course.categoryId === id)
+            if(selectedCategoryData){
+                setCourses(selectedCategoryData)
+                console.log(selectedCategoryData)
+            }
+       
+    }
+
 
     return (
         <div className='w-full flex'>
             <div className="sidebar w-1/4 bg-[#3F0071] text-white">
-            <h2 className='text-2xl p-4'>Categories</h2>
+                <h2 className='text-2xl p-4'>Categories</h2>
                 {
-                    APIcategory.map(category => <li className='list-none p-2 w-full bg-[#FB2576] mx-0 my-5'><Link to={`/courses/${category.id}`}>{category.category}</Link></li>)
+                    APIcategory.map(category => <li onClick={() => { handelcategory(category.id) }} className='category-list list-none p-2 w-full bg-[#FB2576] mx-0 my-5'>{category.category}</li>)
                 }
             </div>
-            <div className="mainsection w-3/4 bg-indigo-400 p-4 grid grid-cols-3 gap-4 ">
-               {
-                courses.map(course => <SingelCourse key={course.id} data={course}></SingelCourse>)
-                // <SingelCourse></SingelCourse>
-               }
+            <div className="mainsection w-3/4 bg-[#FB2576] p-4 grid grid-cols-3 gap-4 ">
+                {
+                    courses.map(course => <SingelCourse key={course.id} data={course}></SingelCourse>)
+                    // <SingelCourse></SingelCourse>
+                }
             </div>
         </div>
     );
--- src/Courses/SingelCourses/SingelCourse.js
+++ src/Courses/SingelCourses/SingelCourse.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const SingelCourse = ({ data }) => {
-    console.log(data)
+    
     return (
     
-        <div className="card card-compact w-full bg-base-100 shadow-xl ">
+        <div className="card card-compact w-full text-white bg-[#000000] shadow-xl ">
             <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
             <div className="card-body">
-                <div className="badge badge-primary">duration: {data.duration}</div>
-                <h2 className="card-title">{data.course_Title}</h2>
+                <div className="badge badge-secondary">duration: {data.duration}</div>
+                <h2 className="card-title text-xl">{data.course_Title}</h2>
+                <h2 className='text-left text-lg'>Author: {data.name}</h2>
                 <div className="card-actions justify-between items-center">
-                    <h2 className='text-3xl text-yellow-400'>$ {data.price}</h2>
-                    <button className="btn btn-primary">More...</button>
+                    <h2 className='text-3xl text-wehite font-bold'>$ {data.price}</h2>
+                    <button className="btn bg-[#FB2576] text-white">More...</button>
                 </div>
             </div>
         </div>
